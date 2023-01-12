5b800e15c69e2e54336ae748744b20c9e9888079
fev <feventolla99@gmail.com>
2023-01-12 22:18:21 +0300

    landingpage

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Field(
-        title: null,
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LandingPage()
+        //  Field(
+        //   title: null,
+        // ),
+        );
   }
 }
