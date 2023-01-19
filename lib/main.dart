fd26e833c4f2746c9f7ee3bbc6bbdcf9ee27dd84
fev <feventolla99@gmail.com>
2023-01-19 12:06:01 +0300

    dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -26,16 +26,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: FarmDashboard()
-        // HomePage(),
-        //     Field(
-        //   title: null,
-        // ),
-        );
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:
+          // FarmDashboard()
+          // HomePage(),
+          Field(
+        title: null,
+      ),
+    );
   }
 }
--- lib/screens/Dashboard/field.dart
+++ lib/screens/Dashboard/field.dart
@@ -681,39 +681,7 @@ class _FieldState extends State<Field> {
                     borderData: FlBorderData(show: false),
                     titlesData: FlTitlesData(
                       show: true,
-                      topTitles: SideTitles(
-                          // showTitles: true,
-                          // reservedSize: 35,
-                          // getTextStyles: (context, value) {
-                          //   return const TextStyle(
-                          //       color: Color.fromARGB(255, 44, 45, 46),
-                          //       fontSize: 16,
-                          //       fontWeight: FontWeight.bold);
-                          // },
-                          // getTitles: (value) {
-                          //   switch (value.toInt()) {
-                          //     case 0:
-                          //       return 'Mon';
-                          //     case 0:
-                          //       return 'Tue';
-                          //     case 5:
-
-//       return 'Wes';
-                          //     case 7:
-                          //       return 'Thur';
-                          //     case 9:
-                          //       return 'Fri';
-                          //     case 11:
-                          //       return 'Sat';
-                          //     case 13:
-                          //       return 'Sun';
-                          //     case 15:
-                          //       return 'Mon';
-                          //   }
-                          //   return '';
-                          // },
-                          // margin: 8
-                          ),
+                      topTitles: SideTitles(),
                       rightTitles: SideTitles(
                         showTitles: true,
                         reservedSize: 35,
@@ -779,6 +747,7 @@ class _FieldState extends State<Field> {
                     minX: 0,
                     lineBarsData: [
                       LineChartBarData(
+                          colors: [Colors.red],
                           dotData: FlDotData(show: false),
                           isCurved: true,
                           spots: [
@@ -796,6 +765,27 @@ class _FieldState extends State<Field> {
                           ])
                     ]),
               ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(left: 100),
+              child: Row(children: <Widget>[
+                Icon(
+                  Icons.circle,
+                  size: 10,
+                  color: Color.fromARGB(255, 218, 13, 13),
+                ),
+                Text(
+                  'Measured Temperature',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff979797),
+                  ),
+                )
+              ]),
+              color: Colors.white,
             ),
             Container(
                 padding: EdgeInsets.only(right: 250, top: 20),
@@ -886,11 +876,262 @@ class _FieldState extends State<Field> {
               ]),
               color: Colors.white,
             ),
+            Row(
+              children: [
+                Container(
+                    padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                    child: const Text(
+                      'Wind',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.left,
+                    )),
+              ],
+            ),
             Container(
-                padding: EdgeInsets.only(top: 50),
-                width: 300,
-                height: 300,
-                color: Colors.black)
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Mon",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "Today",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 105, 107, 109),
+                              fontSize: 10,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Tue",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 50, 51, 52),
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "May 11",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 105, 107, 109),
+                              fontSize: 10,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Wens",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 50, 51, 52),
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "May 13",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 105, 107, 109),
+                              fontSize: 10,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Thur",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 50, 51, 52),
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "May 15",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 105, 107, 109),
+                              fontSize: 10,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Fri",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 50, 51, 52),
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "May 17",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 105, 107, 109),
+                              fontSize: 10,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Sat",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 50, 51, 52),
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "May 19 ",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 105, 107, 109),
+                              fontSize: 10,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                    color: Color.fromARGB(255, 220, 218, 218),
+                    width: 0.5,
+                    style: BorderStyle.solid),
+              ),
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              height: 300,
+              child: LineChart(
+                LineChartData(
+                    gridData: FlGridData(getDrawingHorizontalLine: (value) {
+                      if (value == 0) {
+                        return FlLine(
+                          color: Color.fromARGB(255, 216, 216, 221),
+                          strokeWidth: 0,
+                        );
+                      } else {
+                        return FlLine(
+                          color: Color.fromARGB(255, 208, 208, 212),
+                          strokeWidth: 0.8,
+                        );
+                      }
+                    }, getDrawingVerticalLine: (value) {
+                      return FlLine(color: Color.fromARGB(0, 255, 255, 255));
+                    }),
+                    lineTouchData: LineTouchData(enabled: false),
+                    borderData: FlBorderData(show: false),
+                    titlesData: FlTitlesData(
+                      show: true,
+                      topTitles: SideTitles(),
+                      rightTitles: SideTitles(),
+                      bottomTitles: SideTitles(),
+                      leftTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 35,
+                        getTextStyles: (context, value) {
+                          return const TextStyle(
+                            color: Color.fromARGB(255, 126, 129, 133),
+                            fontSize: 12,
+                          );
+                        },
+                        getTitles: (value) {
+                          switch (value.toInt()) {
+                            case 1:
+                              return '0';
+                            case 2:
+                              return '5';
+                            case 3:
+                              return '10';
+                            case 4:
+                              return '15';
+                            case 5:
+                              return '20';
+                            case 6:
+                              return '25';
+                          }
+                          return '';
+                        },
+                        // margin: 12,
+                      ),
+                    ),
+                    maxX: 6,
+                    maxY: 6,
+                    minY: 0,
+                    minX: 0,
+                    lineBarsData: [
+                      LineChartBarData(
+                          colors: [Colors.black],
+                          dotData: FlDotData(show: false),
+                          isCurved: true,
+                          spots: [
+                            const FlSpot(0.2, 2),
+                            const FlSpot(0.5, 2.2),
+                            const FlSpot(1, 2.5),
+                            const FlSpot(1.5, 3),
+                            const FlSpot(2, 3.5),
+                            const FlSpot(2.5, 4),
+                            const FlSpot(4.4, 4.2),
+                            const FlSpot(4.8, 4.5),
+                            const FlSpot(6, 4.7),
+                            const FlSpot(7, 4.8),
+                            const FlSpot(7.2, 4.9)
+                          ])
+                    ]),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(left: 100),
+              child: Row(children: <Widget>[
+                Icon(
+                  Icons.circle,
+                  size: 10,
+                  color: Color.fromARGB(255, 22, 22, 22),
+                ),
+                Text(
+                  'Measured Temperature',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff979797),
+                  ),
+                )
+              ]),
+              color: Colors.white,
+            ),
+            Container(
+                child: SizedBox(
+              height: 200,
+            ))
           ],
         ),
       ]),
