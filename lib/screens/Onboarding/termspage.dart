96d62243146ab86ea95e27c1de7a09f8b821f7c3
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-02-10 04:51:36 +0300

    Update termspage.dart

--- lib/screens/Onboarding/termspage.dart
+++ lib/screens/Onboarding/termspage.dart
@@ -33,30 +33,7 @@ class TermsPage extends StatelessWidget {
           
           Column(
             
-            children: [
-              Expanded(
-                child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: TermsText(),
-              ),
-              ),
-              SizedBox(height: 20),
-             RaisedButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              color: Color(0xFF275342),
-              child: Text(
-                "AGREE TO TERMS AND CONDITIONS",
-                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
-              ),
-              textColor: Colors.white,
-              highlightColor: Color.fromARGB(255, 119, 72, 72),
-              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 60),
-              onPressed: () {}),
-
-              SizedBox(height: 10) 
-        ]
+           
           ),
         );
   }
