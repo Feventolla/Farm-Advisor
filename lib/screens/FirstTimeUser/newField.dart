1c313dd1aac11a1152d56cef055503fd960f9fbb
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-02-11 22:17:41 +0300

    Update newField.dart

--- lib/screens/FirstTimeUser/newField.dart
+++ lib/screens/FirstTimeUser/newField.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class newFarm extends StatelessWidget {
-  const newFarm({Key? key}) : super(key: key);
+class newField extends StatelessWidget {
+  const newField({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
