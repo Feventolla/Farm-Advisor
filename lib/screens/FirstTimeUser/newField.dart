65f7400fe40ebebea3cd5d5c11836906e02caeeb
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-02-11 22:01:42 +0300

    Add files via upload

---
+++ lib/screens/FirstTimeUser/newField.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class newFarm extends StatelessWidget {
+  const newFarm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Farm"),
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 10, 10, 0),
+            child: Container(
+              child: Text("Field Name"),
+              alignment: Alignment.topLeft,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                filled: true,
+                fillColor: Colors.white,
+                hintText: "Enter field name",
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+            child: Container(child: Text("Altitude Above Sea Level")),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                filled: true,
+                fillColor: Colors.white,
+                hintText: "Enter in meters",
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size(351, 48),
+                  primary: Color.fromARGB(255, 44, 93, 75),
+                  elevation: 3),
+              onPressed: () => {},
+              child: Text("CREATE MY FIRST FARM"))
+        ],
+      )),
+    );
+  }
+}
