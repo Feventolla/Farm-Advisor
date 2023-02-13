f6241fdb235ae20d09f636efc754de8347730526
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-02-13 09:22:51 +0300

    Update profile.dart

--- lib/screens/Dashboard/profile.dart
+++ lib/screens/Dashboard/profile.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:go_router/go_router.dart';
+
 
 class Profile extends StatefulWidget {
   const Profile({
