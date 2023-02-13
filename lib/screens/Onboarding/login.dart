0733dab9acb9835ba5c945390ba716d315799a86
Mekdes Kebede <kebedemekdes289@gmail.com>
2023-02-13 16:12:37 +0300

    additional clean code

--- lib/screens/Onboarding/login.dart
+++ lib/screens/Onboarding/login.dart
@@ -1,13 +1,9 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import 'package:localstorage/localstorage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../models/user.dart';
 
 class PhoneValidator {
@@ -34,11 +30,12 @@ class _LoginState extends State<Login> {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('token') ?? 0;
     if (token != 0) {
+      // ignore: use_build_context_synchronously
       context.go('/home');
     }
   }
 
-  final LocalStorage storage = new LocalStorage('localstorage_app');
+  final LocalStorage storage = LocalStorage('localstorage_app');
   User user = User(country: '', phone: '', token: '');
 
   Future save() async {
@@ -54,16 +51,12 @@ class _LoginState extends State<Login> {
           // 'email': user.email,
           // 'password': user.password
         }));
-    // final token = User.fromJson(json.decode(res.body)).token;
-    // storage.setItem('token', token);
-    // print(storage.getItem('token'));
 
-    // // User.fromJson(json.decode(res.body)).token;
-    // print(res.body);
     if (res.statusCode == 200) {
       final prefs = await SharedPreferences.getInstance();
       prefs.setString('token', User.fromJson(json.decode(res.body)).token);
 
+      // ignore: use_build_context_synchronously
       context.go("/home");
       // Navigator.pushNamed(context, '/home');
     }
@@ -79,7 +72,7 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
+          title: const Text(
             "BACK",
             style: TextStyle(
               color: Color.fromARGB(95, 0, 0, 0),
@@ -89,7 +82,7 @@ class _LoginState extends State<Login> {
           ),
           backgroundColor: Colors.white,
           leading: IconButton(
-            icon: Icon(
+            icon: const Icon(
               Icons.arrow_back,
               color: Color.fromARGB(255, 165, 176, 172),
             ),
@@ -97,127 +90,79 @@ class _LoginState extends State<Login> {
               context.go('/');
             },
           )),
-      body: Container(
-        child: Form(
-          key: formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 15),
-                  child: Text("Login with your phone number",
-                      style: TextStyle(
-                          fontSize: 23,
-                          fontWeight: FontWeight.w500,
-                          color: Color.fromARGB(255, 22, 60, 41))),
+      body: Form(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 15),
+              child: Text("Login with your phone number",
+                  style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 22, 60, 41))),
+            ),
+            // SizedBox(height: 10),
+            Container(
+              margin: const EdgeInsets.only(left: 8, right: 8),
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              child: TextFormField(
+                //key: ValueKey('phoneNo'),
+                //autovalidateMode: AutovalidateMode.onUserInteraction,
+                key: const ValueKey('phone'),
+
+                controller: TextEditingController(text: user.phone),
+                // autovalidateMode: AutovalidateMode.onUserInteraction,
+
+                onChanged: (value) {
+                  user.phone = value;
+                },
+                // onChanged: (value) {
+                //   if (value != null && value.length < 6) {
+                //     setState(() {
+                //       formValid = false;
+                //     });
+                //   } else {
+                //     setState(() {
+                //       formValid = true;
+                //     });
+                //   }
+                // },
+                validator: (value) => PhoneValidator.validate(value),
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  labelText: "Enter your phone number",
                 ),
               ),
-              // SizedBox(height: 10),
-              Container(
-                margin: EdgeInsets.only(left: 8, right: 8),
-                padding: EdgeInsets.only(left: 8, right: 8),
-                child: TextFormField(
-                  //key: ValueKey('phoneNo'),
-                  //autovalidateMode: AutovalidateMode.onUserInteraction,
-                  key: ValueKey('phone'),
+            ),
 
-                  controller: TextEditingController(text: user.phone),
-                  // autovalidateMode: AutovalidateMode.onUserInteraction,
-
-                  onChanged: (value) {
-                    user.phone = value;
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      save();
+                    }
                   },
-                  // onChanged: (value) {
-                  //   if (value != null && value.length < 6) {
-                  //     setState(() {
-                  //       formValid = false;
-                  //     });
-                  //   } else {
-                  //     setState(() {
-                  //       formValid = true;
-                  //     });
-                  //   }
-                  // },
-                  validator: (value) => PhoneValidator.validate(value),
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
-                    labelText: "Enter your phone number",
-                  ),
-                ),
-              ),
-
-              //  Expanded(
-              //   child: Align(
-              //     alignment: Alignment.bottomCenter,
-              //     child: RaisedButton(
-              //       color: formValid ? Color(0xFF275342): Color.fromARGB(255, 213, 223, 219),
-              //       textColor: Colors.white,
-              //       shape: RoundedRectangleBorder(
-              //         borderRadius: BorderRadius.circular(10),
-              //       ),
-              //       highlightColor: Color.fromARGB(255, 119, 72, 72),
-              //       padding:
-              //           EdgeInsets.symmetric(vertical: 15, horizontal: 140),
-              //       onPressed:formValid? () {
-              //         final isValidForm = formKey.currentState!.validate();
-              //         if (isValidForm){}
-              //       }: null,
-              //       child: const Text(
-              //         'Continue',
-              //         style:
-              //             TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-              //       ),
-              //     ),
-              //   ),
-              // ),
-
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-                        save();
-                      }
-                    },
-                    child: Text('Continue'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(255, 119, 72, 72),
-                      onPrimary: Colors.white,
-                      minimumSize: Size(140, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color.fromARGB(255, 119, 72, 72),
+                    onPrimary: Colors.white,
+                    minimumSize: const Size(140, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-
-                    // color: Color(0xFF275342),
-                    // textColor: Colors.white,
-                    // shape: RoundedRectangleBorder(
-                    //   borderRadius: BorderRadius.circular(10),
-                    // ),
-                    // highlightColor: Color.fromARGB(255, 119, 72, 72),
-                    // padding:
-                    //     EdgeInsets.symmetric(vertical: 15, horizontal: 140),
-                    // onPressed: () {
-                    //   if (formKey.currentState!.validate()) {
-                    //     save();
-                    //   }
-                    // },
-                    // child: const Text(
-                    //   'Continue',
-                    //   style:
-                    //       TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                    // ),
                   ),
+                  child: const Text('Continue'),
                 ),
               ),
+            ),
 
-              SizedBox(
-                height: 10,
-              )
-            ],
-          ),
+            const SizedBox(
+              height: 10,
+            )
+          ],
         ),
       ),
     );
--- lib/screens/Onboarding/widgets/countryselector.dart
+++ lib/screens/Onboarding/widgets/countryselector.dart
@@ -1,8 +1,6 @@
 // import 'package:country_code_picker/country_code_picker.dart';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class CountrySelector extends StatefulWidget {
   const CountrySelector({Key? key}) : super(key: key);
@@ -14,16 +12,19 @@ class CountrySelector extends StatefulWidget {
 class _CountrySelectorState extends State<CountrySelector> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-            CountryCodePicker(
-                initialSelection: 'ET',
-                showCountryOnly: false,
-                favorite: ['+251','ET'],),
-            Text("Select country code ", style: TextStyle(fontSize: 16, color: Color.fromARGB(255, 112, 104, 104)),),          
-        ],
-      )
+    return Row(
+      children: [
+        CountryCodePicker(
+          initialSelection: 'ET',
+          showCountryOnly: false,
+          favorite: const ['+251', 'ET'],
+        ),
+        const Text(
+          "Select country code ",
+          style: TextStyle(
+              fontSize: 16, color: Color.fromARGB(255, 112, 104, 104)),
+        ),
+      ],
     );
   }
 }
