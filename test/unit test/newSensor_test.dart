3728ff4a46a1552c73fef6725c424a28e1d7456b
fev <feventolla99@gmail.com>
2023-02-13 15:56:01 +0300

    fixing

--- test/unit test/newSensor_test.dart
+++ test/unit test/newSensor_test.dart
@@ -7,7 +7,7 @@ void main() {
   test('', () {});
 
   test('serial number returns error', () {
-    var result = SerialNumberValidator.validate('');
+    var result = SerialValidator.validate('');
     expect(result, 'serial number can not be empty');
   });
 
@@ -17,17 +17,17 @@ void main() {
   // });
 
   test('default GGD returns error', () {
-    var result = DefaultGGDValidator.validate('');
+    var result = GDDValidator.validate('');
     expect(result, 'default GGD can not be empty');
   });
 
   test('sensor installation date returns error', () {
-    var result = SensorInstallationDateVlidator.validate('');
+    var result = DateValidator.validate('');
     expect(result, 'sensor installation date can not be empty');
   });
 
   test('last cutting date returns error', () {
-    var result = LastCuttingDateValidator.validate('');
+    var result = CuttingValidator.validate('');
     expect(result, 'last cutting date can not be empty');
   });
 }
